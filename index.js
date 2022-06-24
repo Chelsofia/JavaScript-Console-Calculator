b080cf2ccf8e4f04ad76f7d614999fe3fc87877e
Rachel Isaac <86608086+Chelsofia@users.noreply.github.com>
2022-06-24 22:03:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ var num1 = parseInt(prompt("Number 1: "));
 var num2 = parseInt(prompt("Number 2: "));
 
 if(opr == "+"){
+    
     alert(num1 + num2)
+    
 } else if(opr == "-"){
+    
     alert(num1 - num2)
+    
 } else if(opr == "*"){
+    
     alert(num1 * num2)
+
 } else if (opr == "/"){
+    
     alert(num1 / num2)
 };
